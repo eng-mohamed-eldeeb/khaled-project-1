f98885b86d21a8aa74b58782d6781931a4d276ec
eng-mohamed-eldeeb <agnabiieldeeeb@gmial.com>
2024-04-19 15:29:20 +0200

    avdcxz

--- config/puma.rb
+++ config/puma.rb
@@ -16,7 +16,7 @@ workers ENV.fetch("WEB_CONCURRENCY") { 4 }
 # Specifies that the worker count should equal the number of processors in production.
 if ENV["RAILS_ENV"] == "production"
   require "concurrent-ruby"
-  # worker_count = Integer(ENV.fetch("WEB_CONCURRENCY") { Concurrent.physical_processor_count })
+  worker_count = Integer(ENV.fetch("WEB_CONCURRENCY") { Concurrent.physical_processor_count })
   workers worker_count if worker_count > 1
 end
 
